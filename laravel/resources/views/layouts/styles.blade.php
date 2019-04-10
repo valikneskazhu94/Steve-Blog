5216862f3030be46f66b90c0893c46663e3391d8
valikneskazhu94 <valik_ne_skazhu@ukr.net>
2019-04-10 20:40:31 +0300

    Steve-Blog(vol.1)

---
+++ laravel/resources/views/layouts/styles.blade.php
@@ -0,0 +1,15 @@
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="img/favicon.png" type="image/png">
+	<title>Steve Portfolio</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="{{asset(css/bootstrap.css)}}">
+	<link rel="stylesheet" href="{{asset(vendors/linericon/style.css)}}">
+	<link rel="stylesheet" href="{{asset(css/font-awesome.min.css)}}">
+	<link rel="stylesheet" href="{{assetvendors/owl-carousel/owl.carousel.min.css)}}">
+	<link rel="stylesheet" href="{{asset(css/magnific-popup.css)}}">
+	<link rel="stylesheet" href="{{asset(vendors/nice-select/css/nice-select.css)}}">
+	<link rel="stylesheet" href="{{asset(vendors/animate-css/animate.css)}}">
+	<link rel="stylesheet" href="{{asset(vendors/flaticon/flaticon.css)}}">
+	<!-- main css -->
+	<link rel="stylesheet" href="{{asset(css/style.css)}}">
